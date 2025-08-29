1a23e85e66c3248a3e8f682e72428e6ee4da0fc3
Brett Post <post.brettw@gmail.com>
2025-08-29 13:29:42 -0400

    LengthOfLastWord - Attempt 2 - Not the best performance or memory (minimal impact), however the code is very clean.

--- LeetCode/Problems/LengthOfLastWord.cs
+++ LeetCode/Problems/LengthOfLastWord.cs
@@ -4,7 +4,7 @@ namespace LeetCode.Problems
         public int GetWordLength(string s)
         {
             var wordArray = s.Split(' ');
-            return wordArray.Last().Length;
+            return wordArray.Last(w => w.Length > 0).Length;
         }
     }
 }
