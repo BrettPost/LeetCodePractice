b3c46022a98e4837320e082970f82ef1fceb9b6b
BrettPost <bpost121@gmail.com>
2025-09-14 13:51:59 -0700

    FirstOccurrenceOfString - Attempt 1 - Passed - Great Time, okay memory usage

---
+++ LeetCode/Problems/FirstOccurrenceOfString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    internal class FirstOccurrenceOfString
+    {
+        //FirstOccurrenceOfString firstOccurrenceOfString = new();
+        //Console.WriteLine(firstOccurrenceOfString.StrStr("Hello", "World"));
+
+        public int StrStr(string haystack, string needle)
+        {
+            
+            for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                if (haystack.Substring(i, needle.Length).Equals(needle))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,17 +4,5 @@ using LeetCode.Problems;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-ListNode third = new ListNode(3);
-ListNode second = new ListNode(2, third);
-ListNode first = new ListNode(1, second);
-
-ReversedLinkedList reversedLinkedList = new();
-ListNode result = reversedLinkedList.reverseList(first);
-
-while (result.next != null)
-{
-    Console.Write(result.val);
-    result = result.next;
-}
-
-Console.Write(result.val + "\n\n");
+FirstOccurrenceOfString firstOccurrenceOfString = new();
+Console.WriteLine(firstOccurrenceOfString.StrStr("butafdfadkfjsadafdskfja;kldfj", "sad"));
