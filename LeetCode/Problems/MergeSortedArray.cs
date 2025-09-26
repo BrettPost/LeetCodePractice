4d074cce524608a15e73e9a02774d6b6bca9b30f
BrettPost <bpost121@gmail.com>
2025-09-26 12:26:06 -0700

    MergeSortedArray - Attempt 3 - Solution Used - I found thi solution was much better due to how they shifted values while placing the value where it needed to go.

--- LeetCode/Problems/MergeSortedArray.cs
+++ LeetCode/Problems/MergeSortedArray.cs
@@ -16,18 +16,24 @@ namespace LeetCode.Problems
 
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            for (var i = 0; i < n; i++)
+            int k = m + n - 1;
+            int i = m - 1;
+            int j = n - 1;
+
+            while (j >= 0)
             {
-                int j = nums1.Length - 1 - (n - 1 - i);
-                while (j > 0 && nums1[j - 1] > nums2[i])
+                if (i >= 0 && nums1[i] > nums2[j])
                 {
-                    nums1[j] = nums1[j - 1];
+                    nums1[k] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[k] = nums2[j];
                     j--;
                 }
-                nums1[j] = nums2[i];
+                k--;
             }
-
-            //print(nums1);
         }
 
         private void print(int[] nums)
